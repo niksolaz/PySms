bffc23f78fda63f219f172c7bcad422f93dbca0e
niksolaz <solazzo.nicola@gmail.com>
2015-04-03 16:47:35 +0200

    Added views; Added templates

--- PySms/PySms/urls.py
+++ PySms/PySms/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'PySms.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^twilio/',include('twilio.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- PySms/twilio/views.py
+++ PySms/twilio/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Sms
+
+def index(request):
+	latest_sms = Sms.objects.order_by('-pub_date')[:5]
+	context = {'latest_sms': latest_sms}
+	return render(request,'twilio/index.html',context)
