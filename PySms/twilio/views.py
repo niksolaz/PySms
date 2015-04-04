9f35b765ffb16d87e944303b56f827a61e477e87
niksolaz <solazzo.nicola@gmail.com>
2015-04-04 10:56:39 +0200

    added new views

--- PySms/twilio/views.py
+++ PySms/twilio/views.py
@@ -4,6 +4,15 @@ from django.shortcuts import render
 from .models import Sms
 
 def index(request):
+	context = {'message': "Ready to send a message"}
+	if request.method == 'POST':
+		message = request.POST['message']
+		number = request.POST['number']
+		if message and number:
+			context["message"] = "One message has been sent. Ready for another one...."
+	return render(request,'twilio/index.html',context)
+
+def sms_list(request):
 	latest_sms = Sms.objects.order_by('-pub_date')[:5]
 	context = {'latest_sms': latest_sms}
-	return render(request,'twilio/index.html',context)
+	return render(request,'twilio/sms.html',context)
